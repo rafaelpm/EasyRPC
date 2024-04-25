cd49552fdafd6c408d47b2895e695b2ba15f6085
Rafael P Monteiro <rafael.p.monteiro@gmail.com>
2024-04-25 17:47:57 -0300

    Corrigido easy_rpc_bind, não estava procurando função quando existia mais de 10

--- C/EasyRPC/EasyRPC/server/easy_rpc_bind.h
+++ C/EasyRPC/EasyRPC/server/easy_rpc_bind.h
@@ -4,7 +4,7 @@
 
 #define EASY_RPC_FUNCTION_NOT_FOUND -1
 #ifndef EASY_RPC_MAX_FUNCTIONS
-#define EASY_RPC_MAX_FUNCTIONS 10
+	#warning "Defina um valor para EASY_RPC_MAX_FUNCTIONS"
 #endif
 #define EASY_RPC_MAX_SIZE_NAME_FUNCTIONS 128
 struct {
@@ -23,18 +23,25 @@ int getEasyRPC_SearchFunctionIndexByPackage(EasyRPCPackage* package);
 bool easyRPC_ProcessPackageBind(EasyRPCPackage* package) {
 	int index = getEasyRPC_SearchFunctionIndexByPackage(package);
 	if (index == EASY_RPC_FUNCTION_NOT_FOUND) {		
+		#ifdef _CALCULE_TIME
+		printf("easyRPC_ProcessPackageBind = EASY_RPC_FUNCTION_NOT_FOUND\n");
+		#endif
 		return false;
 	}
 	if (easyRPC_RunFunction != NULL) {		
 		easyRPC_RunFunction(package, index);
+	}else{
+		printf("easyRPC_RunFunction = NULL\n");
 	}
 	return true;
 }
 /* ---------------------------------------------------------------------------*/
 bool addEasyRPC_FunctionBind(char* name) {
-	if (EasyRPCBind.totalFunctions + 1 >= EASY_RPC_MAX_FUNCTIONS) {
+	if (EasyRPCBind.totalFunctions + 1 > EASY_RPC_MAX_FUNCTIONS) {
+		//printf("add false (%d) = %s\n",EasyRPCBind.totalFunctions, name);
 		return false;
 	}
+	//printf("add(%d): %s\n",EasyRPCBind.totalFunctions,name);
 	memcpy(&EasyRPCBind.functions[EasyRPCBind.totalFunctions][0], name, strlen(name));
 	EasyRPCBind.totalFunctions++;
 	return true;
@@ -43,6 +50,7 @@ bool addEasyRPC_FunctionBind(char* name) {
 int getEasyRPC_SearchFunctionIndexByName(char* nameFunction) {	
 	uint16_t sizeNameFunction = (uint16_t)strlen(nameFunction);
 	for (int i = 0; i < EasyRPCBind.totalFunctions; i++) {		
+		//printf("SearchName: %s = %s ?\n",&EasyRPCBind.functions[i][0], nameFunction);
 		if (memcmp(&EasyRPCBind.functions[i][0], nameFunction, sizeNameFunction) == 0) {
 			return i;
 		}
